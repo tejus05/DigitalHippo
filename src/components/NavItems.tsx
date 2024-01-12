610e6fe98812bb17c2b1fe3a4de1e76628d86cf5
tejus <thequantumcoder@gmail.com>
2024-01-12 13:26:19 +0530

    updated navbar (without mobile navigation)

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null |number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => {
+    setActiveIndex(null)
+  }); //useOnClickOutside function returns a useEffect by default
+
+  useEffect(() => {
+    const handler = (e:KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    }
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    }
+  },[])
+
+  const close = () => setActiveIndex(null)
+  return ( 
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null)
+          }
+          else {
+            setActiveIndex(i)
+          }
+        }
+
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+            close={close}
+          />
+        )
+      })}
+    </div>
+   );
+}
+ 
+export default NavItems;
