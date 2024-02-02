9af25d468b5a17d7a2cf805a0673f4baf86c8ef6
tejus <thequantumcoder@gmail.com>
2024-02-02 19:22:40 +0530

    build sign-up page

---
+++ src/lib/validators/accountCredentialsValidator.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const AuthCredentialsValidator = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long. " })
+});
